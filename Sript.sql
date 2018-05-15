a79cef6613fd7e14bf4f2cf665a0ed24c87f5b90
zoul97 <39311114+zoul97@users.noreply.github.com>
2018-05-15 13:31:44 -0400

    Add files via upload

--- Sript.sql
+++ Sript.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE bd_Prueba2;
+CREATE DATABASE bd_Prueba2; -- DROP DATABASE bd_Prueba2;
 USE  bd_Prueba2;
 
 CREATE TABLE tipo_usuario(
@@ -7,18 +7,18 @@ CREATE TABLE tipo_usuario(
     PRIMARY KEY(id)
 );
 
-INSERT INTO tipo_usuario VALUE('Administrador');
-INSERT INTO tipo_usuario VALUE('Vendedor');
+-- INSERT INTO tipo_usuario VALUE(null, 'Administrador');
+-- INSERT INTO tipo_usuario VALUE(null, 'Vendedor');
 
 CREATE TABLE usuario(
 	run VARCHAR(50) UNIQUE,
     nombre VARCHAR(100),
     tipo_usuaruo_fk INT REFERENCES tipo_usuario(id),
-    fecha_con DATETIME,
+    fecha_con TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(run)
 );
 
-INSERT INTO usuario VALUE('11-1','admin',1);
+-- INSERT INTO usuario VALUE(null, '11-1','admin',1,);
 
 CREATE TABLE tipo_vivienda(
 	id INT AUTO_INCREMENT,
@@ -26,8 +26,8 @@ CREATE TABLE tipo_vivienda(
     PRIMARY KEY(id)
 );
 
-INSERT INTO tipo_vivienda VALUE('Casa');
-INSERT INTO tipo_vivienda VALUE('Departamento');
+-- INSERT INTO tipo_vivienda VALUE(null, 'Casa');
+-- INSERT INTO tipo_vivienda VALUE(null, 'Departamento');
 
 CREATE TABLE vivienta(
 	rol INT UNIQUE,
@@ -42,10 +42,10 @@ CREATE TABLE vivienta(
 );
 
 CREATE TABLE cliente(
-	run INT UNIQUE,
+	run VARCHAR(100) UNIQUE,
     nombre VARCHAR(100),
     sueldo INT,
-    fecha_reg DATETIME,
+    fecha_reg TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(run)
 );
 
@@ -53,7 +53,7 @@ CREATE TABLE venta(
 	id INT AUTO_INCREMENT,
     usuario_fk INT REFERENCES usuario(run),
     cliente_fk INT REFERENCES cliente(run),
-    fecha DATETIME,
+    fecha TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     vivienda_fk INT REFERENCES vivienta(rol), 
     PRIMARY KEY(id)
 );
